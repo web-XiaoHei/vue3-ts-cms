e649cc92b28ec4723ec738958ce57d4f40484f47
web-XiaoHei <652598147@qq.com>
2022-09-22 00:24:12 +0800

    style: prettier cz router vuex配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,41 @@
 const { defineConfig } = require('@vue/cli-service');
+const AutoImport = require('unplugin-auto-import/webpack');
+const Components = require('unplugin-vue-components/webpack');
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers');
+// const path = require('path');
+
 module.exports = defineConfig({
+  // 1.配置方式一：CLI提供的属性
   transpileDependencies: true,
+  outputDir: './build',
+  // 2，配置方式二：和webpack属性完全一致，最后会进行合并
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: '@/components',
+      },
+    },
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+  },
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components',
+  //   };
+  // },
+  // 3.配置方式三：
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('components', '@/components');
+  // 配置插件
+  //  config.plugin('XXXXX').use("XXXX").tap(options=>{})
+  // },
 });
